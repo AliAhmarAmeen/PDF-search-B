50d5723ee9b4305bc394d84beb932027c5dc1e2d
Ali Ahmar <ahmartech969@gmail.com>
2025-04-19 21:14:01 +0500

    testing to original

--- main.py
+++ main.py
@@ -208,10 +208,3 @@ def health_check():
     return {"status": "App is running", "message": "Health check successful"}
 
 # ----------------------------------------------------------------
-
-if __name__ == "__main__":
-    import uvicorn
-    import os
-
-    port = int(os.environ.get("PORT", 8080))
-    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
